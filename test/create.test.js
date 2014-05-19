073437500e9af2289e742259b137635336cf4750
Johannes Ewald <johannes.ewald@peerigon.com>
2014-05-19 23:51:37 +0200

    Fixed rare fs.write errors

--- test/create.test.js
+++ test/create.test.js
@@ -19,7 +19,7 @@ chai.use(require("chai-as-promised"));
 
 getport = node.lift(getport);
 
-describe.skip("create(config?)", function () {
+describe("create(config?)", function () {
 
     after(slow(function () {
         return phridge.disposeAll();
