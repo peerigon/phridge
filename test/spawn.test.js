e72e3b77a27c27254fd02f14f400f67a6b33c275
Johannes Ewald <johannes.ewald@peerigon.com>
2015-04-28 01:45:09 +0200

    Add test for CLI style config options

--- test/spawn.test.js
+++ test/spawn.test.js
@@ -16,23 +16,10 @@ require("./helpers/setup.js");
 getport = node.lift(getport);
 
 describe("spawn(config?)", function () {
+    var port;
 
-    after(slow(function () {
-        return phridge.disposeAll();
-    }));
-
-    it("should resolve to an instance of Phantom", slow(function () {
-        return expect(spawn()).to.eventually.be.an.instanceOf(Phantom);
-    }));
-
-    it("should pass the provided config to phantomjs", slow(function (done) {
-        var port;
-
-        // We're setting the webdriver option to test if the config is recognized
-        // Setting this option does not make any sense because phantomjs is
-        // unusable with phantomjs in GhostDriver-mode. But it prints a nice
-        // message to the console which causes the promise to be rejected
-        getport(10000)
+    function blockGhostDriverPort() {
+        return getport(10000)
             .then(function (freePort) {
                 var server = net.createServer();
                 var listen = node.lift(server.listen);
@@ -43,7 +30,23 @@ describe("spawn(config?)", function () {
                 // Otherwise the phantomjs processes can't be killed because it doesn't
                 // listen on our commands in GhostDriver-mode.
                 return listen.call(server, freePort);
-            })
+            });
+    }
+
+    after(slow(function () {
+        return phridge.disposeAll();
+    }));
+
+    it("should resolve to an instance of Phantom", slow(function () {
+        return expect(spawn()).to.eventually.be.an.instanceOf(Phantom);
+    }));
+
+    it("should pass the provided config to phantomjs", slow(function (done) {
+        // We're setting the webdriver option to test if the config is recognized
+        // Setting this option does not make any sense because phantomjs is
+        // unusable with phantomjs in GhostDriver-mode. But it prints a nice
+        // message to the console which causes the promise to be rejected
+        blockGhostDriverPort()
             .then(function () {
                 // Prevent PhantomJS from printing a disturbing error message to the console
                 phridge.config.stdout = null;
@@ -51,6 +54,31 @@ describe("spawn(config?)", function () {
 
                 return expect(phridge.spawn({
                     webdriver: "localhost:" + port
+                })).to.be.rejectedWith("PhantomJS is launching GhostDriver...");
+            })
+            .then(function () {
+                phridge.config.stdout = process.stdout;
+                phridge.config.stderr = process.stderr;
+
+                // Give phantomjs some time to exit
+                setTimeout(done, 100);
+            });
+    }));
+
+    it("should also allow CLI style config options", slow(function (done) {
+        // We're setting the webdriver option to test if the config is recognized
+        // Setting this option does not make any sense because phantomjs is
+        // unusable with phantomjs in GhostDriver-mode. But it prints a nice
+        // message to the console which causes the promise to be rejected
+        blockGhostDriverPort()
+            .then(function () {
+                // Prevent PhantomJS from printing a disturbing error message to the console
+                phridge.config.stdout = null;
+                phridge.config.stderr = null;
+
+                return expect(phridge.spawn({
+                    "--load-images": "true",
+                    "--webdriver": "localhost:" + port
                 })).to.be.rejectedWith("PhantomJS is launching GhostDriver...");
             })
             .then(function () {
